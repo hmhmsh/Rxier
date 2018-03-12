7d25913f0cda6a88a88480d8a06cc76e82a89b30
hase <s-hasegawa@infocity.co.jp>
2018-03-12 19:41:41 +0900

    initialize

---
+++ Rxier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rxier
+//
+//  Created by 長谷川瞬哉 on 2018/03/12.
+//  Copyright © 2018年 hmhmsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
