6e153c2e87d2b2fef895242167efa8a4fcaa4d46
hase <s-hasegawa@infocity.co.jp>
2018-03-12 20:07:02 +0900

    feat: PublishSubject・BehavierSubject

---
+++ RxSwift_1/BehavierSubject.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import RxSwift
+
+/**
+* BehaviorSubject は最後の値を覚えていて、subscribeすると即座にそれを最初に通知する Subject
+*/
+
---
+++ RxSwift_1/PublishSubject.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import RxSwift
+
+/**
+* Subjectは、Observableでありつつ、イベントを発生させるための onNext/onError/onCompleted メソッドを提供
+*/
+
+struct Hoge {
+	// Subjectをprivateにする理由：外部からonNext()などを呼べないようにするため
+	private let eventSubject = PublishSubject<Int>()
+	
+	// Observableを公開する
+	var event: Observable<Int> { return eventSubject }
+	
+	func some() {
+		// イベントを通知するときは、Subject.onNext()を呼び出す
+		eventSubject.onNext(1)
+	}
+}
+
+// Usage
+let hoge = Hoge.init()
+
+// Observableのsubscribeを呼び出して、イベントを購読
+let disposable = hoge.event.subscribe(onNext: { (index) in
+	print(index)
+}, onError: { (error) in
+	print(error)
+}, onCompleted: {
+	print("completed")
+})
+
+hoge.some()
+// 購読が必要なくなったら破棄
+disposable.dispose()
+
