3b8421db41f87267a6b90534eff06913e24ae999
hase <s-hasegawa@infocity.co.jp>
2018-03-13 13:47:12 +0900

    feat: BehavierSubjectの実装例追加

--- RxSwift_1/BehavierSubject.playground/Contents.swift
+++ RxSwift_1/BehavierSubject.playground/Contents.swift
@@ -6,4 +6,42 @@ import RxSwift
 /**
 * BehaviorSubject は最後の値を覚えていて、subscribeすると即座にそれを最初に通知する Subject
 */
+struct Presenter {
+	private let buttonHiddenSubject = BehaviorSubject(value: false)
+	var buttonHidden: Observable<Bool> { return buttonHiddenSubject }
+	
+	func start() {
+		buttonHiddenSubject.onNext(true)
+	}
+	
+	func pause() {
+		buttonHiddenSubject.onNext(false)
+	}
+	
+	func stop() {
+		buttonHiddenSubject.onCompleted()
+	}
+	
+	func getValue() -> Bool? {
+		do {
+			return try buttonHiddenSubject.value()
+		} catch let error {
+			print(error)
+			return nil
+		}
+	}
+}
 
+let presenter = Presenter()
+let disposable = presenter.buttonHidden.subscribe(onNext: { (hidden) in
+	print(hidden)
+}, onCompleted: {
+	print("comp")
+})
+
+presenter.start()
+presenter.pause()
+presenter.stop()
+disposable.dispose()
+
+presenter.getValue()
